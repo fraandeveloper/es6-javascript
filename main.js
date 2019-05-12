f64bea99a882b215fdeb8c1a9ec41ed1163ff577
Francisco <franciscobarros754@gmail.com>
2019-05-11 23:29:44 -0300

    criando elementos no DOM

--- main.js
+++ main.js
@@ -2,10 +2,55 @@ function log(param) {
     return console.log(param);
 }
 
-function soma(x, y) {
-    return x + y;
+function insertDom(el) {
+    const containerApp = document.querySelector('#app');
+    containerApp.appendChild(el);
 }
 
-
+function soma(x, y) {
+    return x += y;
+}
 
 log(soma(2, 4));
+
+function write(){
+    console.log('hello world');
+}
+
+//Executa em loop a cada determinado tempo
+// setInterval(write, 4000);
+
+// setTimeout(write, 2000);
+
+const buttonElement = document.querySelector('#app button');
+
+// buttonElement.onclick = () => {
+//     alert('clicado');
+// }
+
+function eventoClick(el) {
+    el.onclick = () => alert('cliquei');
+}
+
+eventoClick(buttonElement);
+
+
+const createLink = document.createElement('a');
+createLink.setAttribute('href','http://teste.com');
+
+const textLink = document.createTextNode('Acesse o link');
+createLink.appendChild(textLink);
+
+const createParagraph = document.createElement('p');
+const textParagraph = document.createTextNode('Texto criado atraves do DOM');
+createParagraph.appendChild(textParagraph);
+
+insertDom(createLink);
+insertDom(createParagraph);
+
+
+
+
+
+
+
